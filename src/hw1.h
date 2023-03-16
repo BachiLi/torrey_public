06cd2b8e6203d04f2b0fba55bdedd6f532f9b00a
Tzu-Mao Li <tzumao@mit.edu>
2023-03-16 12:55:49 -0700

    HW 1.1

--- src/hw1.h
+++ src/hw1.h
@@ -2,4 +2,4 @@
 
 #include "image.h"
 
-Image3 hw_1_1();
+Image3 hw_1_1(const std::vector<std::string> &params);
--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,18 @@
 #include <string>
 
 int main(int argc, char *argv[]) {
-    std::vector<std::string> input_filenames;
+    std::vector<std::string> parameters;
     std::string hw_num;
     for (int i = 1; i < argc; ++i) {
         if (std::string(argv[i]) == "-hw") {
             hw_num = std::string(argv[++i]);
         } else {
-            input_filenames.push_back(std::string(argv[i]));
+            parameters.push_back(std::string(argv[i]));
         }
     }
 
     if (hw_num == "1_1") {
-        Image3 img = hw_1_1();
+        Image3 img = hw_1_1(parameters);
         imwrite("hw_1_1.exr", img);
     }
 
