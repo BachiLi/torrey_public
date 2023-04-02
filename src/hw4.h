f9230682a40e2f2109d9cc94988a875d07ad9cf8
Tzu-Mao Li <tzumao@mit.edu>
2023-04-01 19:06:59 -0700

    Fix typo in header

--- src/hw4.h
+++ src/hw4.h
@@ -4,4 +4,4 @@
 
 Image3 hw_4_1(const std::vector<std::string> &params);
 Image3 hw_4_2(const std::vector<std::string> &params);
-Image3 hw_4_33(const std::vector<std::string> &params);
+Image3 hw_4_3(const std::vector<std::string> &params);
